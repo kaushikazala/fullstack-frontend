7d968bcf59b0c5dc51d610c4ecee5f4f0bd41d5c
kaushikazala <kaushikazala4@gmail.com>
2026-05-01 12:28:36 +0530

    app

---
+++ react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState , useEffect} from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <>
+  <h1>Hello i am kaushika </h1>
+  </>
+  )
+}
+
+export default App
